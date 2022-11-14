20018eb8f7df0f874953a3941108b0d588da8cd4
ChandniJaswalSood <115087423+ChandniJaswalSood@users.noreply.github.com>
2022-11-14 20:43:27 +0530

    Update PasswordEncrypter.java

--- src/printServer/PasswordEncrypter.java
+++ src/printServer/PasswordEncrypter.java
@@ -4,33 +4,17 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.Base64;
-import java.util.Random;
 
 public class PasswordEncrypter
 {
-    private static String my_salt;
-    public static byte[] getSHA(String input, String salt0) throws NoSuchAlgorithmException {
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstance("SHA-512");
         SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        if(salt0==null){
-            random.nextBytes(salt);
-            String str_salt = new String(Base64.getEncoder().encode(salt));
-
-            //System.out.println(str_salt);
-            salt = str_salt.getBytes();
-            my_salt = new String(salt);
-            //System.out.println(str_salt0);
-        }else{
-            salt = salt0.getBytes();
-        }
+        byte[] salt1 = new byte[16];
+        random.nextBytes(salt1);
+        byte[] salt = "helloworld".getBytes();
         md.update(salt);
         return md.digest(input.getBytes());
-    }
-    public static String getSalt(){
-        return my_salt;
     }
 //        Byte to Hex
     public static String toHexString(byte[] encHash) {
@@ -42,7 +26,7 @@ public class PasswordEncrypter
         return hexString.toString();
     }
 
-    public static String getEncryptedPassword(String password,String salt) throws NoSuchAlgorithmException {
-        return toHexString(getSHA(password,salt));
+    public static String getEncryptedPassword(String password) throws NoSuchAlgorithmException {
+        return toHexString(getSHA(password));
     }
 }
