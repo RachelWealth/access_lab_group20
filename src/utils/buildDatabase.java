3d6d62731cf0bded3ea804244a2b07729c2f43f9
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2022-11-18 18:58:22 +0100

    access.db

--- src/utils/buildDatabase.java
+++ src/utils/buildDatabase.java
@@ -12,19 +12,19 @@ public class buildDatabase {
 
     public static void main(String[] args) throws NoSuchAlgorithmException, SQLException, IOException {
         DBManagerProTwo dbhelper = new DBManagerProTwo();
-//        dbhelper.insertRole("administrator","1,2,3,4,5,6,7,8,9");
-//        dbhelper.insertRole("service","4,5,6");
-//        dbhelper.insertRole("technician","7,8,9");
-//        dbhelper.insertRole("pUser","1,2,3,6");
-//        dbhelper.insertRole("user","1,2");
-//
-//        dbhelper.insertUser("Alice","000","administrator");
-//        dbhelper.insertUser("Bob","001","service,technician");
-//        dbhelper.insertUser("Cecilia","010","pUser");
-//        dbhelper.insertUser("David","011","user");
-//        dbhelper.insertUser("Erica","100",null);
-//        dbhelper.insertUser("Fres","101","user");
-//        dbhelper.insertUser("George","110",null);
+        dbhelper.insertRole("administrator","1,2,3,4,5,6,7,8,9");
+        dbhelper.insertRole("service","1,2,4,5,6");
+        dbhelper.insertRole("technician","1,2,7,8,9");
+        dbhelper.insertRole("pUser","1,2,3,6");
+        dbhelper.insertRole("user","1,2");
+
+        dbhelper.insertUser("Alice","000","administrator");
+        dbhelper.insertUser("Bob","001","service,technician");
+        dbhelper.insertUser("Cecilia","010","pUser");
+        dbhelper.insertUser("David","011","user");
+        dbhelper.insertUser("Erica","100",null);
+        dbhelper.insertUser("Fres","101","user");
+        dbhelper.insertUser("George","110",null);
 //
 //        ResultSet rs = dbhelper.searchPermission("Alice");
 //        if (rs.next())
